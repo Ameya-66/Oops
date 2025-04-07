1ae5d1ba43db38303c3e85b8f5f0f37f12f14f78
Ameya-66 <ameyaprashantmahajan@gmail.com>
2025-04-07 18:07:31 +0530

    TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -1,6 +1,26 @@
-// TaskManager - Main Class
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
-    public static void main(String[] args) {
-        System.out.println("Hello from TaskManager");
+    private final List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void deleteTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        }
+    }
+
+    public void markTaskAsDone(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markDone();
+        }
     }
 }
