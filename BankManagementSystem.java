09ab96ae56d992d0e2fb708fba82b12c304b56d7
Ameya-66 <ameyaprashantmahajan@gmail.com>
2025-04-07 18:10:07 +0530

    BankManagementSystem.java

--- BankManagementSystem.java
+++ BankManagementSystem.java
@@ -1,6 +1,20 @@
-// BankManagementSystem - Main Class
-public class BankManagementSystem {
-    public static void main(String[] args) {
-        System.out.println("Hello from BankManagementSystem");
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+    private Map<Integer, Account> accounts = new HashMap<>();
+
+    public void createAccount(String holder, int number) {
+        accounts.put(number, new Account(holder, number));
+    }
+
+    public Account getAccount(int number) {
+        return accounts.get(number);
+    }
+
+    public void showAllAccounts() {
+        for (Account acc : accounts.values()) {
+            System.out.println(acc);
+        }
     }
 }
