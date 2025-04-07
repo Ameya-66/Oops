fc80c72c6424d28073899c92051a5c7b37b1e72c
Ameya-66 <ameyaprashantmahajan@gmail.com>
2025-04-07 18:13:24 +0530

    LibraryManagementSystem.java

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,6 +1,36 @@
-// LibraryManagementSystem - Main Class
-public class LibraryManagementSystem {
-    public static void main(String[] args) {
-        System.out.println("Hello from LibraryManagementSystem");
+public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void borrowBook() {
+        isAvailable = false;
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + (isAvailable ? " (Available)" : " (Borrowed)");
     }
 }
